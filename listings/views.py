e486dacd49f2833ecf0a030961fcb1017242da40
jlagun <kubalaguna@gmail.com>
2018-12-30 01:33:34 +0100

    almost finished

--- listings/views.py
+++ listings/views.py
@@ -1,16 +1,64 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Listing
+from realtors.models import Realtor
+from listings.choices import state_choices, price_choices, bedroom_choices
 
 def index(request):
-    listings = Listing.objects.all()
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)
+    paginator = Paginator(listings, 3)
+    page = request.GET.get('page')
+    paged_listings = paginator.get_page(page)
     context = {
-        'listings': listings
+        'listings': paged_listings
     }
     return render(request, 'listings/listings.html', context)
 
 def listing(request, listing_id):
-    return render(request, 'listings/listing.html')
+    listing = get_object_or_404(Listing, pk=listing_id)
+    realtors_mvp = Realtor.objects.all().filter(is_mvp=True)
+    context = {
+    'listing' : listing,
+    'realtors_mvp' : realtors_mvp
+    }
+    return render(request, 'listings/listing.html', context)
 
 def search(request):
-    return render(request, 'listings/search.html')
+    query = Listing.objects.order_by('-list_date')
+    # Keywords
+    if 'keywords' in request.GET:
+        keywords = request.GET['keywords']
+        if keywords:
+            query = query.filter(description__icontains=keywords)
+
+    # City
+    if 'city' in request.GET:
+        city = request.GET['city']
+        if city:
+            query = query.filter(city__iexact=city)
+
+    # State
+    if 'state' in request.GET:
+        state = request.GET['state']
+        if state:
+            query = query.filter(state__iexact=state)
+
+    # Bedrooms
+    if 'bedrooms' in request.GET:
+        bedrooms = request.GET['bedrooms']
+        if bedrooms:
+            query = query.filter(bedrooms__iexact=bedrooms)
+
+    # Price
+    if 'price' in request.GET:
+        price = request.GET['price']
+        if price:
+            query = query.filter(price__lte=price)
+    context = {
+        'listings': query,
+        'state_choices': state_choices,
+        'bedroom_choices': bedroom_choices,
+        'price_choices': price_choices,
+        'values': request.GET
+    }
+    return render(request, 'listings/search.html', context)
