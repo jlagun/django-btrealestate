15bb9a8706ca6bbae67a2f4e075f57892aaeeb03
jlagun <kubalaguna@gmail.com>
2018-12-30 02:06:27 +0100

    final

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages, auth
 from django.contrib.auth.models import User
+from contacts.models import Contact
 def logout(request):
     if request.method == "POST":
         auth.logout(request)
@@ -56,4 +57,8 @@ def register(request):
         return render(request, 'accounts/register.html')
 
 def dashboard(request):
-    return render(request, 'accounts/dashboard.html')
+    user_contacts = Contact.objects.order_by('-contact_date').filter(user_id=request.user.id)
+    context = {
+        'contacts': user_contacts
+    }
+    return render(request, 'accounts/dashboard.html', context)
